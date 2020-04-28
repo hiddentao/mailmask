cb246bbd04d24ba38470621c338b685e327b38db
Ramesh Nair <ram@hiddentao.com>
2020-04-28 17:51:33 +0100

    refactor(web): analytics on in all modes, but we track app mode

--- packages/web/pages/_document.js
+++ packages/web/pages/_document.js
@@ -26,22 +26,21 @@ export default class MyDocument extends Document {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no"
           />
           <link rel='stylesheet' href='https://unpkg.com/@fortawesome/fontawesome-svg-core@1.2.28/styles.css' crossOrigin='anonymous'></link>
-          {appConfig.APP_MODE === 'live' ? (
-            <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
-              var _paq = window._paq || [];
-              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-              _paq.push(["setDoNotTrack", true]);
-              _paq.push(['trackPageView']);
-              _paq.push(['enableLinkTracking']);
-              (function() {
-                var u="https://hiddentao.matomo.cloud/";
-                _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '1']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/hiddentao.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-              })();
-            `}}></script>
-          ) : null}
+          <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `
+            var _paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(["setDoNotTrack", true]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            _paq.push(['setCustomVariable', 1, 'appMode', '${appConfig.APP_MODE}', 'visit']);
+            (function() {
+              var u="https://hiddentao.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.defer=true; g.src='//cdn.matomo.cloud/hiddentao.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `}}></script>
         </Head>
         <body>
           <Main />
