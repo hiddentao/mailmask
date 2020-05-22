e71403954e169612aee93c6fd8dd163b5aee73bf
Ramesh Nair <ram@hiddentao.com>
2020-05-22 12:53:39 +0100

    chore(web): disable unneeded line for now

--- packages/web/pages/_document.js
+++ packages/web/pages/_document.js
@@ -26,11 +26,11 @@ export default class MyDocument extends Document {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no"
           />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-          <link rel="manifest" href="/static/site.webmanifest" />
-          <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#000000" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          {/* <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" /> */}
           <meta name="apple-mobile-web-app-title" content="Mailmask" />
           <meta name="application-name" content="Mailmask" />
           <meta name="msapplication-TileColor" content="#f7fff7" />
